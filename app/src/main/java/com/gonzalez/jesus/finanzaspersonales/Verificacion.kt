84bf39fd80b4cbc03c70938da2bc154a9f21a228
Joel Luna <57472255+JoelLuna@users.noreply.github.com>
2021-04-14 13:22:51 -0700

    Sena

---
+++ app/src/main/java/com/gonzalez/jesus/finanzaspersonales/Verificacion.kt
@@ -0,0 +1,11 @@
+package com.gonzalez.jesus.finanzaspersonales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Verificacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verificacion)
+    }
+}

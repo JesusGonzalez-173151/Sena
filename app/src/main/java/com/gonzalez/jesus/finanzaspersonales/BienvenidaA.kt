5440faf90cde25e5e4e9f81b4c04f5707623235c
JesusGonzalez-173151 <jesusi.gonzalez@potros.itson.edu.mx>
2021-05-23 03:22:22 -0700

    bienvenidas abc
    Necesita acomodo de imagen superior
    "No es fragmento solo simulara avance" sin animacion de deslizar

---
+++ app/src/main/java/com/gonzalez/jesus/finanzaspersonales/BienvenidaA.kt
@@ -0,0 +1,11 @@
+package com.gonzalez.jesus.finanzaspersonales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BienvenidaA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bienvenida)
+    }
+}
